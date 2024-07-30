53be90bb801b74caee4134622402364f061f1088
R.G. Wood <ric@grislyeye.com>
2024-07-30 12:22:39 +0100

    Add: link previews

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,9 @@
-const project = require('./package.json');
 const replaceLink = require('markdown-it-replace-link');
 const path = require("path");
-// const linkPreviewPlugin = require("./lib/link-preview-md-plugin.js");
+const cheerio = require('cheerio');
+const slugify = require('slugify')
 
-function byIndex(left, right) {
-  const a = left.data.index ? Number.parseInt(left.data.index) : 0;
-  const b = right.data.index ? Number.parseInt(right.data.index) : 0;
-  return a - b;
-}
+const project = require('./package.json');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
@@ -22,7 +18,7 @@ module.exports = function (eleventyConfig) {
     {
       'node_modules/@lit': 'vendor/@lit',
       'node_modules/vellum-doc': 'vendor/vellum-doc',
-      'node_modules/link-preview': 'vendor/link-preview',
+      'node_modules/@grislyeye/link-preview': 'vendor/link-preview',
       'node_modules/magick.css': 'vendor/magick.css',
       'node_modules/normalize.css': 'vendor/normalize.css'
     }
@@ -48,6 +44,36 @@ module.exports = function (eleventyConfig) {
           return link;
         }
       })
-      // .use(linkPreviewPlugin);
+      .use(automaticallyGenerateHeadingIds);
   });
+
+  eleventyConfig.addTransform("link-preview", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      const $ = cheerio.load(content)
+      $('a[href^="#"]').wrap('<link-preview></link-preview>');
+      return $.html()
+    }
+
+    return content;
+	});
 };
+
+function byIndex(left, right) {
+  const a = left.data.index ? Number.parseInt(left.data.index) : 0;
+  const b = right.data.index ? Number.parseInt(right.data.index) : 0;
+  return a - b;
+}
+
+function automaticallyGenerateHeadingIds(md) {
+  const defaultRender = md.renderer.rules.heading_open || function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.heading_open = function (tokens, idx, opts, env, self) {
+    const heading = tokens[idx]
+    const content = tokens[idx+1]
+    const id = slugify(content.content).toLowerCase()
+    heading.attrSet('id', id)
+    return defaultRender(tokens, idx, opts, env, self)
+  };
+}
